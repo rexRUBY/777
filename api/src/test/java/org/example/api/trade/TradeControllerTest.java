246dd630dc18add160880829604d83265146ff90
Sangmin Lee <sart5534@gmail.com>
2024-10-29 17:15:00 +0900

    test: 특정 거래 목록 조회 성공

--- api/src/test/java/org/example/api/trade/TradeControllerTest.java
+++ api/src/test/java/org/example/api/trade/TradeControllerTest.java
@@ -16,9 +16,12 @@ import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.util.List;
+
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -58,7 +61,32 @@ class TradeControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.cryptoSymbol").value("BTC"))
                 .andExpect(jsonPath("$.amount").value(10.0))
-                .andExpect(jsonPath("$.billType").value("BUY"))  // tradeType으로 변경
+                .andExpect(jsonPath("$.billType").value("BUY"))
                 .andExpect(jsonPath("$.price").value(5000L));
+    }
+
+    @Test
+    public void 특정_거래_목록_조회_성공() throws Exception {
+        // given
+        long cryptoId = 1L;
+        List<TradeResponseDto> responseDtoList = List.of(
+                new TradeResponseDto("BTC", 10.0, "BUY", 5000L),
+                new TradeResponseDto("ETH", 5.0, "SELL", 3000L)
+        );
+
+        when(tradeService.getTradeList(any(), eq(cryptoId))).thenReturn(responseDtoList);
+
+        // when & then
+        mockMvc.perform(get("/api/v1/cryptos/{cryptoId}/trades", cryptoId)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].cryptoSymbol").value("BTC"))
+                .andExpect(jsonPath("$[0].amount").value(10.0))
+                .andExpect(jsonPath("$[0].billType").value("BUY"))
+                .andExpect(jsonPath("$[0].price").value(5000L))
+                .andExpect(jsonPath("$[1].cryptoSymbol").value("ETH"))
+                .andExpect(jsonPath("$[1].amount").value(5.0))
+                .andExpect(jsonPath("$[1].billType").value("SELL"))
+                .andExpect(jsonPath("$[1].price").value(3000L));
     }
 }
