f97992a4979700714bc02c96475eddc38c90ced6
Sangmin Lee <sart5534@gmail.com>
2024-10-29 21:21:30 +0900

    test: 비밀번호 변경 성공

--- api/src/test/java/org/example/api/user/UserServiceTest.java
+++ api/src/test/java/org/example/api/user/UserServiceTest.java
@@ -3,6 +3,7 @@ package org.example.api.user;
 import org.example.api.user.service.UserService;
 import org.example.common.common.dto.AuthUser;
 import org.example.common.common.exception.InvalidRequestException;
+import org.example.common.user.dto.request.UserChangePasswordRequest;
 import org.example.common.user.dto.request.UserWithdrawRequest;
 import org.example.common.user.dto.response.UserResponse;
 import org.example.common.user.entity.User;
@@ -18,6 +19,7 @@ import org.springframework.test.util.ReflectionTestUtils;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -89,5 +91,28 @@ public class UserServiceTest {
         });
 
         assertEquals("비밀번호가 틀렸습니다.", exception.getMessage());
+    }
+
+    @Test
+    public void 비밀번호_변경_성공() {
+        // given
+        AuthUser authUser = AuthUser.from(1L, "test@example.com");
+        User mockUser = new User();
+        ReflectionTestUtils.setField(mockUser, "id", authUser.getId());
+        ReflectionTestUtils.setField(mockUser, "password", "oldEncodedPassword");
+
+        when(userRepository.findById(authUser.getId())).thenReturn(Optional.of(mockUser));
+
+        // 설정된 passwordEncoder.matches() 호출에 따른 예외 방지
+        doReturn(true).when(passwordEncoder).matches("oldPassword", "oldEncodedPassword"); // 이전 비밀번호 일치 확인
+        doReturn(false).when(passwordEncoder).matches("NewPassword1", "oldEncodedPassword"); // 새 비밀번호가 다름 확인
+        when(passwordEncoder.encode("NewPassword1")).thenReturn("newEncodedPassword");
+
+        // when
+        UserChangePasswordRequest passwordRequest = new UserChangePasswordRequest("oldPassword", "NewPassword1");
+        userService.changePassword(authUser, passwordRequest);
+
+        // then
+        assertEquals("newEncodedPassword", mockUser.getPassword());
     }
 }
