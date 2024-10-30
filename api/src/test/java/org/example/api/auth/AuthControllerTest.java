74f4ae329f01c5da8c7cd301b75a18f4bce6ab61
Sangmin Lee <sart5534@gmail.com>
2024-10-30 16:02:10 +0900

    test: 비밀번호 초기화 성공

--- api/src/test/java/org/example/api/auth/AuthControllerTest.java
+++ api/src/test/java/org/example/api/auth/AuthControllerTest.java
@@ -3,6 +3,7 @@ package org.example.api.auth;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.api.auth.controller.AuthController;
 import org.example.api.auth.service.AuthService;
+import org.example.common.auth.dto.request.ResetPasswordRequest;
 import org.example.common.auth.dto.request.SigninRequest;
 import org.example.common.auth.dto.request.SignupRequest;
 import org.example.common.auth.dto.response.SigninResponse;
@@ -19,11 +20,13 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 
 @ExtendWith(MockitoExtension.class)
 class AuthControllerTest {
@@ -74,5 +77,19 @@ class AuthControllerTest {
                         .content(new ObjectMapper().writeValueAsString(signinRequest)))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.bearerToken").value("generatedToken"));
+    }
+
+    @Test
+    public void 비밀번호_초기화_성공() throws Exception {
+        // given
+        ResetPasswordRequest resetPasswordRequest = new ResetPasswordRequest("test@example.com", "resetToken", "NewValid123@");
+
+        doNothing().when(authService).resetPassword(any(ResetPasswordRequest.class));
+
+        // when & then
+        mockMvc.perform(patch("/api/v1/auth/reset-password")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(resetPasswordRequest)))
+                .andExpect(status().isOk());
     }
 }
