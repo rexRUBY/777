2da4cf6d10f684a2fe98a9215ab88b33a7580add
Yeseo Park <ye_2002@naver.com>
2024-11-15 09:04:42 +0900

    Refector : 코드리뷰 피드백 반영

--- common/src/main/java/org/example/common/user/entity/User.java
+++ common/src/main/java/org/example/common/user/entity/User.java
@@ -45,35 +45,35 @@ public class User extends Timestamped {
     private boolean userStatus = true; // 유저 상태 (true: 활성, false: 탈퇴)
 
     @OneToMany(mappedBy = "followingUser", cascade = CascadeType.ALL, orphanRemoval = true)
-//나를 팔로우 하는사람들과의 팔로우목록 // 내가 신청받는것
-//    @BatchSize(size = 20)
+    // 나를 팔로우 하는사람들과의 팔로우목록, 내가 신청받는것
+    // @BatchSize(size = 20)
     @Fetch(FetchMode.SUBSELECT)
     private List<Subscriptions> subscriptionsBeingFollowed = new ArrayList<>();
 
     @OneToMany(mappedBy = "followerUser", cascade = CascadeType.ALL, orphanRemoval = true)
-//내가 팔로우 하는사람들과의 팔로우 목록 // 내가 신청한것
-//    @BatchSize(size = 10)
+    // 내가 팔로우 하는사람들과의 팔로우 목록, 내가 신청한것
+    // @BatchSize(size = 10)
     @Fetch(FetchMode.SUBSELECT)
     private List<Subscriptions> subscriptionsIFollow = new ArrayList<>();
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-//    @BatchSize(size = 10)
+    // @BatchSize(size = 10)
     @Fetch(FetchMode.SUBSELECT)
     private List<WalletHistory> walletHistoryList = new ArrayList<>();
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-//    @BatchSize(size = 10)
+    // @BatchSize(size = 10)
     @Fetch(FetchMode.SUBSELECT)
     private List<Trade> tradeList = new ArrayList<>();
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-//    @BatchSize(size = 10)
+    // @BatchSize(size = 10)
     @Fetch(FetchMode.SUBSELECT)
     @BatchSize(size = 2)
     private List<Wallet> walletList = new ArrayList<>();
 
     // 생성자: 필수 필드만 포함
-    private User(String email, String password, String name) {
+    public User(String email, String password, String name) {
         this.email = email;
         this.password = password;
         this.name = name;
@@ -100,5 +100,11 @@ public class User extends Timestamped {
 
     public void changeProcess(){
         this.processed=true;
+    }
+
+    public void updateUser(String email, String password, String name) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
     }
 }
