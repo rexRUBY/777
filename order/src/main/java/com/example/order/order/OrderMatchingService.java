8f9a537d4b201d6fd04914f4827dff49e0c3ce79
jhwook <jhwooook@naver.com>
2024-11-15 19:00:36 +0900

    Fix : 주문 매칭 로직 변경(재귀 -> 반복문)

--- order/src/main/java/com/example/order/order/OrderMatchingService.java
+++ order/src/main/java/com/example/order/order/OrderMatchingService.java
@@ -1,5 +1,6 @@
 package com.example.order.order;
 
+import com.example.order.order.dto.OrderMatchResult;
 import com.example.order.trade.service.TradeService;
 import com.example.order.wallet.WalletService;
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -51,7 +52,7 @@ public class OrderMatchingService {
             String symbol = (String) data.get("symbol");
             boolean isTradeStatus = false;
 
-            tryMatchOrder(stringPrice, stringAmount, userId, tradeType, symbol, stringAmount, isTradeStatus);
+            tryMatchOrder(stringPrice, stringAmount, userId, tradeType, symbol);
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -65,66 +66,81 @@ public class OrderMatchingService {
             String amount,
             Long userId,
             String orderType,
-            String symbol,
-            String initialAmount,
-            boolean isTradeStatus
+            String symbol
     ) {
 
         String oppositeOrderType = (orderType.equals(BUY_ORDER_KEY)) ? SELL_ORDER_KEY : BUY_ORDER_KEY;
         String orderKey = symbol + "_" + oppositeOrderType;
 
-        Set<ZSetOperations.TypedTuple<Object>> oppositeOrders = redisTemplate
-                .opsForZSet()
-                .rangeByScoreWithScores(
-                        orderKey,
-                        Double.parseDouble(price),
-                        Double.parseDouble(price)
-                );
+        double remainingAmount = Double.parseDouble(amount);
 
-        if (oppositeOrders != null && !oppositeOrders.isEmpty()) {
-            // 가장 최근 주문 가져오기
-            ZSetOperations.TypedTuple<Object> latestOppositeOrder = oppositeOrders.iterator().next();
+        OrderMatchResult result = new OrderMatchResult(remainingAmount, false);
 
-            String oppositeOrderId = (String) latestOppositeOrder.getValue();
-            String priceStr = (String) stringRedisTemplate.opsForHash().get(oppositeOrderId, "price");
+        while(remainingAmount > 0) {
+            Set<ZSetOperations.TypedTuple<Object>> oppositeOrders = redisTemplate
+                    .opsForZSet()
+                    .rangeByScoreWithScores(
+                            orderKey,
+                            Double.parseDouble(price),
+                            Double.parseDouble(price)
+                    );
 
-            log.info(orderKey + " orderId: " + oppositeOrderId);
+            if (oppositeOrders != null && !oppositeOrders.isEmpty()) {
+                // 가장 최근 주문 가져오기
+                ZSetOperations.TypedTuple<Object> latestOppositeOrder = oppositeOrders.iterator().next();
 
-            // 가격이 매칭되는지 확인
-            if ((orderType.equals(BUY_ORDER_KEY) && Double.parseDouble(price) >= Double.parseDouble(priceStr)) ||
-                    (orderType.equals(SELL_ORDER_KEY) && Double.parseDouble(priceStr) >= Double.parseDouble(price))) {
-                processOrderMatch(latestOppositeOrder, amount, Double.parseDouble(priceStr), orderType, userId, symbol, initialAmount, isTradeStatus);
-                return;
+                String oppositeOrderId = (String) latestOppositeOrder.getValue();
+                String priceStr = (String) stringRedisTemplate.opsForHash().get(oppositeOrderId, "price");
+
+//            log.info(orderKey + " orderId: " + oppositeOrderId);
+
+                // 가격이 매칭되는지 확인
+                if ((orderType.equals(BUY_ORDER_KEY) && Double.parseDouble(price) >= Double.parseDouble(priceStr)) ||
+                        (orderType.equals(SELL_ORDER_KEY) && Double.parseDouble(priceStr) >= Double.parseDouble(price))) {
+
+                     remainingAmount = processOrderMatch(
+                            latestOppositeOrder,
+                            remainingAmount,
+                            Double.parseDouble(priceStr),
+                            orderType,
+                            userId,
+                            symbol
+                    );
+
+                     result.setTradeStatus(true);
+
+                    if(remainingAmount > 0) {
+                        continue;
+                    }
+                }
+
             }
-        }
 
-        if (isTradeStatus) {
-            sendAlarmMessage(userId, symbol, String.valueOf(initialAmount), orderType);
-        }
+            if(result.getIsTradeStatus()) {
+                sendAlarmMessage(userId, symbol, String.valueOf(Double.parseDouble(amount) - Double.parseDouble(String.valueOf(remainingAmount))), orderType);
+            }
 
-        // 매칭되지 않으면 새로운 주문 추가
-        log.info("매칭 결과 없음 " + orderType + " ORDER 추가 " + orderType + " Price: " + price + ", " + orderType + " Amount: " + amount);
-        addOrder(price, amount, orderType, userId, symbol);
+            // 매칭되지 않으면 새로운 주문 추가
+//        log.info("매칭 결과 없음 " + orderType + " ORDER 추가 " + orderType + " Price: " + price + ", " + orderType + " Amount: " + amount);
+            addOrder(price, String.valueOf(remainingAmount), orderType, userId, symbol);
+
+            break;
+        }
     }
 
-    private void processOrderMatch(
+    private double processOrderMatch(
             ZSetOperations.TypedTuple<Object> order,
-            String amount,
+            double requestAmount,
             double price,
             String orderType,
             Long userId,
-            String symbol,
-            String initialAmount,
-            boolean isTradeStatus
+            String symbol
     ) {
         String orderId = (String) order.getValue();
         double availableAmount = Double.parseDouble((String) stringRedisTemplate.opsForHash().get(orderId, "amount"));
-        double requestAmount = Double.parseDouble(amount);
+
         String timestampStr = (String) stringRedisTemplate.opsForHash().get(orderId, "timestamp");
         Double timestamp = Double.valueOf(timestampStr);
-
-        log.info("requestAmount: " + requestAmount);
-        log.info("availableAmount: " + availableAmount);
 
         String oppositeUserIdStr = (String) stringRedisTemplate.opsForHash().get(orderId, "userId");
         Long oppositeUserId = Long.valueOf(oppositeUserIdStr);
@@ -132,7 +148,7 @@ public class OrderMatchingService {
 
         if (availableAmount >= requestAmount) {
             stringRedisTemplate.opsForHash().put(orderId, "amount", String.valueOf(availableAmount - requestAmount));
-            log.info(availableAmount + "개 중 " + requestAmount + "개 처리됨 => " + (availableAmount - requestAmount) + "개 남음");
+//            log.info(availableAmount + "개 중 " + requestAmount + "개 처리됨 => " + (availableAmount - requestAmount) + "개 남음");
 
             // 유저 지갑 업데이트
             walletService.updateWallet(userId, oppositeUserId, price, requestAmount, orderType, symbol);
@@ -143,31 +159,27 @@ public class OrderMatchingService {
             // 알림 전송
             sendAlarmMessage(userId, symbol, String.valueOf(requestAmount), orderType);
             sendAlarmMessage(oppositeUserId, symbol, String.valueOf(requestAmount), oppositeOrderType);
+
+            return 0;
         } else {
 
-            String orderKey = symbol + oppositeOrderType;
-
-            log.info("orderType: " + orderType);
-            log.info("orderKey: " + orderKey);
+            String orderKey = symbol + "_" + oppositeOrderType;
 
             redisTemplate.opsForZSet().remove(orderKey, orderId);
 
-            log.info(requestAmount + "개 중 " + availableAmount + "개 처리됨 => 주문 삭제 완료");
+//            log.info(requestAmount + "개 중 " + availableAmount + "개 처리됨 => 주문 삭제 완료");
 
             // 유저 지갑 업데이트
-            walletService.updateWallet(userId, oppositeUserId, price, requestAmount, orderType, symbol);
+            walletService.updateWallet(userId, oppositeUserId, price, availableAmount, orderType, symbol);
 
             // 로그 생성
-            tradeService.saveLog(userId, oppositeUserId, price, requestAmount, orderType, symbol, orderId, timestamp);
+            tradeService.saveLog(userId, oppositeUserId, price, availableAmount, orderType, symbol, orderId, timestamp);
 
             // 알림
-            sendAlarmMessage(oppositeUserId, symbol, String.valueOf(requestAmount), oppositeOrderType);
+            sendAlarmMessage(oppositeUserId, symbol, String.valueOf(availableAmount), oppositeOrderType);
 
-            String restAmount = String.valueOf(requestAmount - availableAmount);
-            log.info(restAmount + "개 추가 " + (orderType.equals(BUY_ORDER_KEY) ? "BUY" : "SELL") + " 매칭 시작");
-
-            isTradeStatus = true;
-            tryMatchOrder(String.valueOf(price), restAmount, userId, orderType, symbol, initialAmount, isTradeStatus);
+//            log.info(restAmount + "개 추가 " + (orderType.equals(BUY_ORDER_KEY) ? "BUY" : "SELL") + " 매칭 시작");
+            return requestAmount - availableAmount;
         }
     }
 
@@ -177,7 +189,7 @@ public class OrderMatchingService {
         double doublePrice = Double.parseDouble(stringPrice);
         double score = doublePrice;
         String orderId = UUID.randomUUID().toString();
-        String saveOrderKey = symbol + orderKey;
+        String saveOrderKey = symbol + "_" + orderKey;
 
         redisTemplate.opsForZSet().add(saveOrderKey, orderId, score);
         redisTemplate.opsForHash().put(orderId, "price", stringPrice);
@@ -187,8 +199,10 @@ public class OrderMatchingService {
     }
 
     public void sendAlarmMessage(Long userId, String symbol, String amount, String orderType) {
-        kafkaTemplate.send("alarm-topic-" + userId,
-                symbol + "가 " + amount + "개 " +
-                        (orderType.equals(BUY_ORDER_KEY) ? "매수 " : "매도 ") + "체결되었습니다.");
+//        kafkaTemplate.send("alarm-topic-" + userId,
+//                symbol + "가 " + amount + "개 " +
+//                        (orderType.equals(BUY_ORDER_KEY) ? "매수 " : "매도 ") + "체결되었습니다.");
+        log.info(userId + "의 " + symbol + "가 " + amount + "개 " +
+                (orderType.equals(BUY_ORDER_KEY) ? "매수 " : "매도 ") + "체결되었습니다.");
     }
 }
