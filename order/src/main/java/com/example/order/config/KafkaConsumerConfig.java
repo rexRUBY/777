75b49dd524791a2fba1ecfe3ac2d046e84002189
jhwook <jhwooook@naver.com>
2024-11-20 19:01:20 +0900

    Feat : Kafka EOS 적용

--- order/src/main/java/com/example/order/config/KafkaConsumerConfig.java
+++ order/src/main/java/com/example/order/config/KafkaConsumerConfig.java
@@ -8,6 +8,7 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -30,6 +31,7 @@ public class KafkaConsumerConfig {
     public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
         ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
+        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL); // 수동 커밋 설정
         return factory;
     }
 }
